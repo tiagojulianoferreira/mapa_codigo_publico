92666d414366d4e043542ddb700468bc345fa013
Tiago Ferreira <tiagojulianoferreira@gmail.com>
2025-08-03 20:11:10 -0300

    Atualização automática do mapa de código público

--- consulta.sh
+++ consulta.sh
@@ -1,14 +1,12 @@
 #!/bin/bash
-
-# Define o caminho completo para o interpretador Python do seu ambiente virtual
-PYTHON_VENV="/usr/bin/python3"  # Altere para o caminho correto do seu Python virtual
-
-# Usa o caminho direto para o interpretador Python
-$PYTHON_VENV ./filtra_palavras_chave.py
+#python github_busca_ampla.py
 sleep 2
-$PYTHON_VENV ./filtrar_idiomas.py
+/bin/python filtra_palavras_chave.py
 sleep 2
-
+python filtrar_idiomas.py
+sleep 2 
+#python clusterizador.py
+#sleep 2
 git add .
 git commit -m "Atualização automática do mapa de código público"
 git push origin main
