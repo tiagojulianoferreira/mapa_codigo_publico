ea4c3faf672e78c4f2282f8ea6241f1ede0821d5
Tiago Ferreira <tiagojulianoferreira@gmail.com>
2025-08-03 20:02:02 -0300

    Atualização automática do mapa de código público

--- consulta.sh
+++ consulta.sh
@@ -1,21 +1,14 @@
 #!/bin/bash
 
-# O caminho completo para o diretório do projeto
-PROJETO_DIR="./"
+# Define o caminho completo para o interpretador Python do seu ambiente virtual
+PYTHON_VENV="./.venv/bin/python"
 
-# Ativa o ambiente virtual
-source "$PROJETO_DIR/.venv/bin/activate"
-
-# Use o caminho completo para os scripts
+# Usa o caminho direto para o interpretador Python
+$PYTHON_VENV filtra_palavras_chave.py
 sleep 2
-python "$PROJETO_DIR/filtra_palavras_chave.py"
-sleep 2
-python "$PROJETO_DIR/filtrar_idiomas.py"
+$PYTHON_VENV filtrar_idiomas.py
 sleep 2
 
-# Os comandos do git também podem precisar de caminhos absolutos, dependendo de onde o script é executado
-# Exemplo: git -C "$PROJETO_DIR" add .
-# Mas se você for usar o 'cd' da primeira opção, não precisa se preocupar com isso.
 git add .
 git commit -m "Atualização automática do mapa de código público"
 git push origin main
