2e8a5caee0fee0dad186358bb29c9a98acbd06e5
Tiago Ferreira <tiagojulianoferreira@gmail.com>
2025-08-03 20:10:27 -0300

    Atualização automática do mapa de código público

--- consulta.sh
+++ consulta.sh
@@ -4,9 +4,9 @@
 PYTHON_VENV="/usr/bin/python3"  # Altere para o caminho correto do seu Python virtual
 
 # Usa o caminho direto para o interpretador Python
-$PYTHON_VENV filtra_palavras_chave.py
+$PYTHON_VENV ./filtra_palavras_chave.py
 sleep 2
-$PYTHON_VENV filtrar_idiomas.py
+$PYTHON_VENV ./filtrar_idiomas.py
 sleep 2
 
 git add .
